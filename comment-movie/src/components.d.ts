1ee51b59cfe249ffa0a11ec100cfff9098dcc9c2
thywillbedone <dfmartinez8909@gmail.com>
2022-05-26 00:03:06 +0200

    basic stuff

--- comment-movie/src/components.d.ts
+++ comment-movie/src/components.d.ts
@@ -7,6 +7,9 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface CommentMovie {
+        "btndown": string;
+        "btnup": string;
+        "color": string;
     }
     interface MyComponent {
         /**
@@ -43,6 +46,9 @@ declare global {
 }
 declare namespace LocalJSX {
     interface CommentMovie {
+        "btndown"?: string;
+        "btnup"?: string;
+        "color"?: string;
     }
     interface MyComponent {
         /**
--- comment-movie/src/components/comment-movie/comment-movie.tsx
+++ comment-movie/src/components/comment-movie/comment-movie.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop, State, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'comment-movie',
@@ -6,11 +6,34 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class CommentMovie {
+  @Prop() btnup: string;
+  @Prop() btndown: string;
+  @Prop() color: string;
+
+  @State() score = 0;
+
+  // @Event() didReset: EventEmitter;
+
+  up() {
+    this.score++;
+  }
+
+  down() {
+    this.score--;
+  }
+
+  reset() {
+    this.score = 0;
+    // this.didReset.emit(true)
+  }
 
   render() {
     return (
       <Host>
-        <slot></slot>
+        <slot name="title"></slot>
+        <button onClick={() => this.up()}>{this.btnup}</button>
+        <div>{this.score}</div>
+        <button onClick={() => this.down()}>{this.btndown}</button>
       </Host>
     );
   }
